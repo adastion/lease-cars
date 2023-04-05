56af89b00e73ce0ec7b1b79409729ea85227c08e
Andrey Sery <andrey.72006@gmail.com>
2023-04-05 23:56:46 +0300

    Swiper (lib)

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
+// swiper
+import Swiper from 'swiper/bundle';
 
+// init Swiper:
+const swiper = new Swiper('.slider', {
+  autoplay: {
+    delay: 3000,
+  },
+
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  loop: true,
+});
