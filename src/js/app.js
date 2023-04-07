0689109c87cb8c8b10ccffff9dc51a931c738225
Andrey Sery <andrey.72006@gmail.com>
2023-04-08 01:53:10 +0300

    Fixing job-description. Adding vidio.

--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,17 @@ import Swiper from "swiper/bundle";
 
 // init Swiper:
 const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 
-
   effect: "fade",
   fadeEffect: {
     crossFade: true,
+  },
+
+  autoplay: {
+    delay: 3000,
   },
 });
