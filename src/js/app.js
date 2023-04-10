1b3aba429ff6a162d9f006b7bbd63af54b8e79ed
Andrey Sery <andrey.72006@gmail.com>
2023-04-10 22:25:37 +0300

    Fix cards (link)

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
+//tabs
+const tabBtn = document.querySelectorAll(".cotalog__btn-tab");
+const tabContent = document.querySelectorAll(".cotalog__content-list");
+
+tabBtn.forEach((event) => {
+  event.preventDefault();
+  event.addEventListener('click', show);
+});
+
+function show(event) {
+  const tabTarget = event.currentTarget;
+
+  tabBtn.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  tabTarget.classList.add("active");
+}
+
 // swiper
 import Swiper from "swiper/bundle";
 
